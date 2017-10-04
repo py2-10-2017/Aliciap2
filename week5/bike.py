a9da700ea6104b7a58b4081b9493f4ad5b9b940b
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2017-10-03 20:32:05 -0400

    Adding first assignment and folder

---
+++ week5/bike.py
@@ -0,0 +1,29 @@
+class Bike(object):
+	def __init__(self, price, max_speed): 
+		self.price = price
+		self.max_speed = max_speed
+		self.miles = 0
+
+	def displayInfo(self):
+		print self.price, self.max_speed, self.miles
+		return self
+
+	def ride(self):
+		self.miles += 10
+		print "Riding", self.miles
+		return self
+
+	def reverse(self): 
+		self.miles -= 5
+		print "Reversing", self.miles
+		return self
+
+redBike = Bike(200, "25mph")
+redBike.reverse().displayInfo().ride()
+print redBike
+
+blueBike = Bike(300, "15mph").ride().ride().reverse().reverse().displayInfo()
+print blueBike
+
+yellowBike = Bike(1000, "30mph").reverse().reverse().reverse().displayInfo()
+print yellowBike
