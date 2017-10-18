e1f38c6375d999be38e041c38e7fdcddb9d942a3
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2017-10-18 07:06:17 -0400

    Recent edits

--- week7/belt_main/belt_reviewer/settings.py
+++ week7/belt_main/belt_reviewer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.belt_review',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
