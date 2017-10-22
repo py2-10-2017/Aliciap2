52949e9b4711c79566edcdabc2f91d41bcdad131
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2017-10-22 05:42:23 -0400

    Working messages code and overall setup

--- week7/loginreg_main/apps/login/urls.py
+++ week7/loginreg_main/apps/login/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
 from . import views
+
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^register$', views.register),
-    url(r'^login$', views.login),
     url(r'^register_view$', views.register_view),
     url(r'^login_view$', views.login_view),
-    url(r'^success$', views.success),
-    url(r'^logout', views.logout),
-    url(r'^create_note', views.create)
+    url(r'^register$', views.register),
+    url(r'^success$', views.success)
 ]
