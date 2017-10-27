ce197f658941751fca187ead9ee5ae6ded370565
Alicia <AleeWeb@users.noreply.github.com>
2017-10-27 12:54:14 -0400

    Fixed project level urls

--- week7/belt_main/belt_reviewer/urls.py
+++ week7/belt_main/belt_reviewer/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 urlpatterns = [
-   url(apps.belt_review.urls);
+  url(r'^', include('apps.login.urls')),
+  url(r'^', include('apps.reviews.urls'))
 ]
+
+
