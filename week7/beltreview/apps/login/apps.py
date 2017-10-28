fba08dffb0bc2e8b62b502cc593618caa83ccb54
Alicia Alexander <aliciaalexander@Alicias-MacBook-Pro.local>
2017-10-27 22:27:43 -0400

    Redo Belt Review for final exam warm-up. Delete older belt

--- week7/beltreview/apps/login/apps.py
+++ week7/beltreview/apps/login/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class BeltReviewConfig(AppConfig):
-    name = 'belt_review'
+class LoginConfig(AppConfig):
+    name = 'login'
--- week7/beltreview/beltreview/wsgi.py
+++ week7/beltreview/beltreview/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for belt_reviewer project.
+WSGI config for beltreview project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belt_reviewer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beltreview.settings")
 
 application = get_wsgi_application()
